8cf0272de8b2145735c6d9df906bbbe03937aa8b
Alejandro Castro <castroal96@gmail.com>
2021-02-16 21:15:00 -0500

    chnaged workouts function with aggreate method

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,77 +3,91 @@ const { Mongoose } = require("mongoose");
 //
 const Workout = require("../models/workout");
 
-API.getLastWorkout
+// API.getLastWorkout
+// router.get("/api/workouts", (req, res) => {
+// 	Workout.find({})
+// 		.then(dbResult => res.json(dbResult))
+// 		.catch(err => console.log(err))
+// });
+
 router.get("/api/workouts", (req, res) => {
-	Workout.find({})
-		.then(dbResult => res.json(dbResult))
-		.catch(err => console.log(err))
-});
-
-API.addExercise()
-router.put("/api/workouts/:id", (req, res) => {
-	let workoutSchema = {
-		day: new Date(),
-		exercises: [
-			{
-				type: req.body.type,
-				name: req.body.name,
-				duration: req.body.duration || null,
-				weight: req.body.weight || null,
-				reps: req.body.reps || null,
-				sets: req.body.sets || null,
-				distance: req.body.distance || null
-			}
-		]
-	}
-
-	Workout.create(workoutSchema).then(result => {
-		console.log(result);
-		res.status(200).json({
-			'message': 'Record inserted successfully'
-		});
-	}).catch(error => {
-		console.log(error);
-		res.status(500).json({
-			'message': 'Error inserting record'
-		});
-	});
-});
-
-router.put("/api/workouts/:id", (req, res) => {
-	console.log(req.body, req.params.id);
-	console.log(req.params);
-	Workout.findOneAndUpdate(
-	  { _id: req.params.id },
-	  {
+	Workout.aggregate()
+	  .addFields({ totalDuration: { $sum: "$exercises.duration" } })
   
-		$push: { exercises: req.body },
-	  },
-	  { new: true, runValidators: true }
-	)
 	  .then((data) => {
-  
 		res.json(data);
 	  })
 	  .catch((err) => {
 		res.json(err);
 	  });
+  
   });
+  
+
+// API.addExercise()
+// router.put("/api/workouts/:id", (req, res) => {
+// 	let workoutSchema = {
+// 		day: new Date(),
+// 		exercises: [
+// 			{
+// 				type: req.body.type,
+// 				name: req.body.name,
+// 				duration: req.body.duration || null,
+// 				weight: req.body.weight || null,
+// 				reps: req.body.reps || null,
+// 				sets: req.body.sets || null,
+// 				distance: req.body.distance || null
+// 			}
+// 		]
+// 	}
+
+// 	Workout.create(workoutSchema).then(result => {
+// 		console.log(result);
+// 		res.status(200).json({
+// 			'message': 'Record inserted successfully'
+// 		});
+// 	}).catch(error => {
+// 		console.log(error);
+// 		res.status(500).json({
+// 			'message': 'Error inserting record'
+// 		});
+// 	});
+// });
+
+// router.put("/api/workouts/:id", (req, res) => {
+// 	console.log(req.body, req.params.id);
+// 	console.log(req.params);
+// 	Workout.findOneAndUpdate(
+// 	  { _id: req.params.id },
+// 	  {
+  
+// 		$push: { exercises: req.body },
+// 	  },
+// 	  { new: true, runValidators: true }
+// 	)
+// 	  .then((data) => {
+  
+// 		res.json(data);
+// 	  })
+// 	  .catch((err) => {
+// 		res.json(err);
+// 	  });
+//   });
 
 
 
-// API.createWorkout
-router.post("/api/workouts/", ({ body }, res) => {
-	//This will create a workout based on the scheema
+// // API.createWorkout
+// router.post("/api/workouts/", ({ body }, res) => {
+// 	//This will create a workout based on the scheema
 
-	Workout.create(body)
-		.then(result => {
-			res.json(result);
-		})
-		.catch(err => {
-			res.json(err);
-		});
-});
+// 	Workout.create(body)
+// 		.then(result => {
+// 			res.json(result);
+// 		})
+// 		.catch(err => {
+// 			res.json(err);
+// 		});
+// });
 
 
 module.exports = router;
