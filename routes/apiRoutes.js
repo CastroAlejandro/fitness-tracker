b5762a8a94f109bf772c36812df22439d27b0637
Alejandro Castro <castroal96@gmail.com>
2021-02-16 21:13:20 -0500

    added api route for excercise id

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,6 +10,35 @@ router.get("/api/workouts", (req, res) => {
 		.catch(err => console.log(err))
 });
 
+API.addExercise()
+router.put("/api/workouts/:id", (req, res) => {
+	let workoutSchema = {
+		day: new Date(),
+		exercises: [
+			{
+				type: req.body.type,
+				name: req.body.name,
+				duration: req.body.duration || null,
+				weight: req.body.weight || null,
+				reps: req.body.reps || null,
+				sets: req.body.sets || null,
+				distance: req.body.distance || null
+			}
+		]
+	}
+
+	Workout.create(workoutSchema).then(result => {
+		console.log(result);
+		res.status(200).json({
+			'message': 'Record inserted successfully'
+		});
+	}).catch(error => {
+		console.log(error);
+		res.status(500).json({
+			'message': 'Error inserting record'
+		});
+	});
+});
 
 
 
